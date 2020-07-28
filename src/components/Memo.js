cf8e4d01eb814402fa7b70fcb4f00d91d78c6ba4
qkdgywls0503 <qkdgywls0503@naver.com>
2020-07-28 21:41:23 +0900

    end

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 const Container = styled.div`
   width: 500px;
@@ -22,6 +23,10 @@ const RegDate = styled.div`
   padding: 10px;
 `;
 
+const RefKey = styled.h3`
+  padding: 10px;
+`;
+
 class Memo extends React.Component {
   render() {
     return (
@@ -29,8 +34,35 @@ class Memo extends React.Component {
         <Title>{this.props.title}</Title>
         <Content>{this.props.content}</Content>
         <RegDate>{this.props.regDate}</RegDate>
+        <button onClick={() => this._deleteBtnHandler(this.props.refKey)}>
+          삭제
+        </button>
       </Container>
     );
   }
+
+  _deleteBtnHandler = async (refKey) => {
+    const inputData = {
+      refKey,
+    };
+
+    await axios
+      .post(
+        "/api/deleteBtnHandler",
+        { params: { inputData } },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        if (response.data === 1) {
+          alert("삭제 성공");
+        } else {
+          alert("삭제 실패");
+        }
+      });
+  };
 }
 export default Memo;
